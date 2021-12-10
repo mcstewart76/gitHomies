72d4433696b29e545f3ccc43c318d2fc4f1af534
Ernesto Tham <ernesto.tham@kbslp.com>
2021-12-10 13:47:06 -0500

    new updates JS and html css for wall page

--- assets/js/github.js
+++ assets/js/github.js
@@ -13,67 +13,97 @@ function searchFriends(username){
 }
 
 
-function gitHub_GetUserData_Async(githubuser){
-    var url = 'https://api.github.com/users/'+githubuser
-
-    try {
-    	const response = await fetch(URL)
-    	const data = await response.json()
-        console.log(data)
-    } catch (err) {
-       console.log(err)
-    }
-
-    return data
-
+async function gitHub_GetUserData_Async(githubuser){
+    var url = `https://api.github.com/users/${githubuser}`
+    // console.log(url)
+    let response = await fetch(url)
 }
 
+async function gettestData(githubuser) {
+    let response = await fetch(`https://api.github.com/users/${githubuser}`);
+    let data = await response.json()
+    return data;
+  }
+  
+  
+  
+//   async function writetoconsole(){
+//         // getData is a promise
 
-function gitHub_GetFollowers_Async(githubuser){
+//         console.log("test1")
+//         console.log(gettestData('ernestotham')
+
+//       }
+  
+
+
+
+// gitHub_GetUserData_Async('ernestotham').then((resp) => { console.log("got user data")
+// console.log(resp)
+// console.log("done")})
+
+
+
+async function gitHub_GetFollowers_Async(githubuser){
     var url = 'https://api.github.com/users/'+githubuser+'/followers'
 
     try {
-    	const response = await fetch(URL)
+    	const response = await fetch(url)
     	const data = await response.json()
         console.log(data)
+        return data
     } catch (err) {
        console.log(err)
     }
 
-    return data
+    
 
 }
 
 
-function gitHub_GetFollowing_Async(githubuser){
+async function gitHub_GetFollowing_Async(githubuser){
     var url = 'https://api.github.com/users/'+githubuser+'/following'
 
     try {
-    	const response = await fetch(URL)
+    	const response = await fetch(url)
     	const data = await response.json()
         console.log(data)
     } catch (err) {
        console.log(err)
     }
 
-    return data
+    
 
 }
 
 
-function gitHub_GetUserRepos_Async(githubuser){
+async function gitHub_GetUserRepos_Async(githubuser){
     var url = 'https://api.github.com/users/'+githubuser+'/repos'
 
     try {
-    	const response = await fetch(URL)
+    	const response = await fetch(url)
     	const data = await response.json()
         console.log(data)
     } catch (err) {
        console.log(err)
     }
 
-    return data
+    
 
 }
 
 
+
+
+async function showAvatar(user) {
+
+    
+    // read github user
+    let githubResponse = await fetch(`https://api.github.com/users/${user}`);
+    let githubUser = await githubResponse.json();
+  
+   
+    return githubUser.avatar_url;
+  }
+  
+//   var avatar = showAvatar('ernestotham');
--- assets/js/wall.js
+++ assets/js/wall.js
@@ -2,15 +2,19 @@
 //create a search textbox that allows users to search gitlabs api to find other users
 
 //https://api.github.com/users/[user]
-submitBtn = document.getElementById("search-btn")
-.click('#search-btn', friendMatch())
-function friendMatch(input){
-    searchBox = document.getElementById("friend-search")
-    searchBox.innerHTML = '';
-    if (input == ''){
-        return "clicked";
-    }
-}
+
+
+// submitBtn = document.getElementById("search-btn")
+// .click('#search-btn', friendMatch())
+// function friendMatch(input){
+//     searchBox = document.getElementById("friend-search")
+//     searchBox.innerHTML = '';
+//     if (input == ''){
+//         return "clicked";
+//     }
+// }
+
+
 // let icon = '';
 // fetch('https://api.github.com/users/' + input).then(
 //     function(response){
@@ -19,3 +23,22 @@ function friendMatch(input){
 //     })//.then(function(data)){add list items here}
 
 // }
+
+
+var card = $("#avatarimage")
+
+showAvatar('ernestotham').then((val) => {
+    console.log(val)
+    card.append(`<a class="navbar-brand" href="#"><img id=homieavatar src=${val} alt="avatar"></a>`)
+    
+    });
+    
+
+
+    
+
+
+
+
+
+    
