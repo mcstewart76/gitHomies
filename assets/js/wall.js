8a84ef92e7bb9746cf73a92a3405d991c07473fc
mcstewart76 <mcstewart76@gmail.com>
2021-12-09 20:00:11 -0500

    Added js file for wall page

---
+++ assets/js/wall.js
@@ -0,0 +1,4 @@
+//create a search textbox that allows users to search githubs api to find other users
+//create a search textbox that allows users to search gitlabs api to find other users
+
+//https://api.github.com/users/[user]
