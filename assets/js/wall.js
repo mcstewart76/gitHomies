e8b6f06bbff75cc4a6ac630859fb9ad7b8c1a212
mcstewart76 <mcstewart76@gmail.com>
2021-12-09 20:56:17 -0500

    Added some basic logic for a fetch api for friends search

--- assets/js/wall.js
+++ assets/js/wall.js
@@ -2,3 +2,14 @@
 //create a search textbox that allows users to search gitlabs api to find other users
 
 //https://api.github.com/users/[user]
+function friendMatch(input){
+    if (input == ''){
+        return;
+    }
+fetch('https://api.github.com/users/' + input).then(
+    function(response){
+        console.log(response)
+        return response.json();  
+    })//.then(function(data)){add list items here}
+
+}
