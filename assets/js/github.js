994d8d0adbfd3f5bdd1621a8014c37b854f7c153
Ernesto Tham <ernesto.tham@kbslp.com>
2021-12-10 00:40:32 -0500

    updated github.js

--- assets/js/github.js
+++ assets/js/github.js
@@ -11,3 +11,69 @@ function searchFriends(username){
         githubUserData = data;
     })
 }
+
+
+function gitHub_GetUserData_Async(githubuser){
+    var url = 'https://api.github.com/users/'+githubuser
+
+    try {
+    	const response = await fetch(URL)
+    	const data = await response.json()
+        console.log(data)
+    } catch (err) {
+       console.log(err)
+    }
+
+    return data
+
+}
+
+
+function gitHub_GetFollowers_Async(githubuser){
+    var url = 'https://api.github.com/users/'+githubuser+'/followers'
+
+    try {
+    	const response = await fetch(URL)
+    	const data = await response.json()
+        console.log(data)
+    } catch (err) {
+       console.log(err)
+    }
+
+    return data
+
+}
+
+
+function gitHub_GetFollowing_Async(githubuser){
+    var url = 'https://api.github.com/users/'+githubuser+'/following'
+
+    try {
+    	const response = await fetch(URL)
+    	const data = await response.json()
+        console.log(data)
+    } catch (err) {
+       console.log(err)
+    }
+
+    return data
+
+}
+
+
+function gitHub_GetUserRepos_Async(githubuser){
+    var url = 'https://api.github.com/users/'+githubuser+'/repos'
+
+    try {
+    	const response = await fetch(URL)
+    	const data = await response.json()
+        console.log(data)
+    } catch (err) {
+       console.log(err)
+    }
+
+    return data
+
+}
+
+
