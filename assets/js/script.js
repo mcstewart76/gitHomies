4e24e1f69d4f888760cf47a690c537d71b92890f
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2021-12-15 20:31:11 -0500

    Demo Page
    Demo Page finished product

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+ document.getElementById("signinbtn").onclick = function () {
+        window.location.href = "logInPage.UI.html";
+ };
+
+ document.getElementById("registerbtn").onclick = function () {
+    window.location.href = "registration.html";
+};
